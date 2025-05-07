ec756b52715dd55de286e2bbc319d6b939ec8d9c
Osinachukwu Nnanna <contact2amondz24@gmail.com>
2025-05-07 20:21:04 +0100

    about us changes added images

--- src/components/Aboutus/Aboutus.jsx
+++ src/components/Aboutus/Aboutus.jsx
@@ -3,6 +3,25 @@ import './AboutUs.css'
 import { motion } from 'framer-motion'
 import { FaTooth } from 'react-icons/fa'
 
+const AboutUsCard = ({ audesc }) => {
+    return (
+        <>
+            <div className="info-card">
+                <div className="icon"><FaTooth /></div>
+                <p className="card-title">{audesc}</p>
+            </div>
+        </>
+    )
+}
+
+const MissionList = ({ missioncaption, missionimg, missionimgcaption }) => {
+    return (
+        <>
+            <li className="missionlistcaption"><img draggable="false" className="mlistimg" src={missionimg} alt={missionimgcaption} />{missioncaption}</li>
+        </>
+    )
+}
+
 const Aboutus = () => {
     return (
         <>
@@ -16,24 +35,23 @@ const Aboutus = () => {
                     <div className="d-flex flex-column lh-1">
                         <span className="pgtitle fw-bold txtcolor3 mt-4 cm-width">OUR COMMITMENT</span>
                         <span className="pgsubtitle fw-normal txtcolor2">DENTAL EDUCATION FOR ALL</span>
+                        <span className="mission-title txtcolor3 mt-4 mb-4">Our Mission</span>
+                        <p className="customgrey mission-text">"Experience gentle, professional care the gives you confidence to smile. Progessive dental solutions for your family."</p>
+                        <ul className="list-unstyled d-flex flex-column">
+                            <MissionList missionimgcaption="doctor silhoutte" missionimg="mission1.png" missioncaption="Expert Team" />
+                            <MissionList missionimgcaption="people silhoutte" missionimg="mission2.png" missioncaption="Family Focus" />
+                            <MissionList missionimgcaption="badge" missionimg="mission3.png" missioncaption="Quality Care" />
+                            <MissionList missionimgcaption="medical help" missionimg="mission4.png" missioncaption="Gentle Care" />
+                        </ul>
                     </div>
-                </section>
 
-                <section className="about-section c-pad">
-                    <p className="mission-text txtcolor3">"To provide exceptional dental care with a commitment to comfort, quality, and lasting oral health."</p>
+                    <div className="text-center mt-5 mb-5">
+                        <p className="mission-text2 txtcolor3">"To provide exceptional dental care with a commitment to comfort, quality, and lasting oral health."</p>
 
-                    <div className="card-container">
-                        <div className="info-card">
-                            <div className="icon"><FaTooth /></div>
-                            <p className="card-title">State-of-the-art Technology</p>
-                        </div>
-                        <div className="info-card">
-                            <div className="icon"><FaTooth /></div>
-                            <p className="card-title">Experienced Dentists</p>
-                        </div>
-                        <div className="info-card">
-                            <div className="icon"><FaTooth /></div>
-                            <p className="card-title">Comfortable Environment</p>
+                        <div className="a-cards">
+                            <AboutUsCard audesc="State-of-the-art Technology" />
+                            <AboutUsCard audesc="Experienced Dentists" />
+                            <AboutUsCard audesc="Comfortable Environment" />
                         </div>
                     </div>
                 </section>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const ProductButton = () => {
     return (
 
         <button className="heroproduct-btn fw-semibold c-shadow align-items-center">
-            <img src="store-icon.png" alt="White Box" className="icon"/>
+            <img src="store-icon.png" alt="White Box" className="p-icon" />
             Check out our Products
         </button>
     );
@@ -18,7 +18,7 @@ const HeroSection = () => {
         <div className="hero-container d-flex align-items-center">
             <div className="hero-text pe-5">
                 <h1 className="lh-0p72 fw-bold">
-                    <span className="txtcolor3 t-fontsize">SMILE</span>{' '}<br />
+                    <span className="txtcolor3 t-fontsize">SMILE</span><br />
                     <span className="txtcolor2 st-fontsize">BRIGHTER</span>
                 </h1>
                 <p className="fw-medium text-muted mt-4 p-fontsize p-color">
@@ -26,13 +26,8 @@ const HeroSection = () => {
                     Progressive dental solutions for your entire family.
                 </p>
                 <ProductButton />
-                {/* <div className="mt-3">
-                    <a href="#" className="txtcolor2 mt-3 small text-decoration-none">
-                        Try new patient special →
-                    </a>
-                </div> */}
             </div>
-            <img draggable="false" className="hero-image imgfx"  src="1BG.png" alt="" />
+            <img draggable="false" className="hero-image imgfx" src="1BG.png" alt="" />
         </div>
     );
 };
