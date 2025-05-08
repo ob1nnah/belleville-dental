8b014bdbfdf5994690cd1971c6b30d604f12b673
Osinachukwu Nnanna <contact2amondz24@gmail.com>
2025-05-08 22:45:09 +0100

    changes img path in gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import './Gallery.css'
 
+//media
+import gallery1 from '/gallery/1.png'
+import gallery2 from '/gallery/2.png'
+import gallery3 from '/gallery/3.png'
+import gallery4 from '/gallery/4.png'
+import gallery5 from '/gallery/5.png'
+import gallery6 from '/gallery/6.png'
+
 const GalleryCard = ({ gimg, galt }) => {
     return (
         <div className="g-card col-sm-4">
@@ -18,14 +26,14 @@ const Gallery = () => {
                     <span className="pgsubtitle fw-normal txtcolor2 m-auto">A glimpse into our practice</span>
                 </div>
                 <div className="row mt-4 g-card ">
-                    <GalleryCard gimg="gallery/1.png" caption="Office and Observation Room" />
-                    <GalleryCard gimg="gallery/2.png" caption="Dental Lab" />
-                    <GalleryCard gimg="gallery/3.png" caption="Staff Working" />
+                    <GalleryCard gimg={gallery1} caption="Office and Observation Room" />
+                    <GalleryCard gimg={gallery2} caption="Dental Lab" />
+                    <GalleryCard gimg={gallery3} caption="Staff Working" />
                 </div>
                 <div className="row mt-4 g-card ">
-                    <GalleryCard gimg="gallery/4.png" caption="Toothbrush" />
-                    <GalleryCard gimg="gallery/5.png" caption="Dentist Chair" />
-                    <GalleryCard gimg="gallery/6.png" caption="Operation" />
+                    <GalleryCard gimg={gallery4} caption="Toothbrush" />
+                    <GalleryCard gimg={gallery5} caption="Dentist Chair" />
+                    <GalleryCard gimg={gallery6} caption="Operation" />
                 </div>
             </section>
         </div>
