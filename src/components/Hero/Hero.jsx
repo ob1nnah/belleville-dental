a18c6794cb89a0ca018a311c268096b5b9eaf277
calculus guy <sakariyauabdullateef993@gmail.com>
2025-05-12 18:47:14 +0100

    fixes applied

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import HeroImage from '/1BG.png';
 import WhiteStore from '/store-icon.png';
 import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
 
